6c49c13fe572f3f952768815aa2e9ba32d26f40a
virtusademo <virtusademo@gmail.com>
2017-11-21 11:52:08 +0400

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ All code is under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
 
 <?php
 $db_host = "localhost";  // server to connect to.
-$db_name = "login";  // the name of the database.
-$db_user = "database username";  // mysql username to access the database with.
-$db_pass = "database password";  // mysql password to access the database with.
+$db_name = "db";  // the name of the database.
+$db_user = "dbuser";  // mysql username to access the database with.
+$db_pass = "dbpassword";  // mysql password to access the database with.
 $db_table = "users";    // the table that this script will set up and use.
 ?>
